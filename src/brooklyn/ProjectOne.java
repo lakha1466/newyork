7279cc48d1e0c0c096d88efb51f94999a9e1ad0e
lakha <lakha@DESKTOP-TF1KP1G.myfiosgateway.com>
2021-03-23 20:34:17 -0400

    Show Me My Code

--- src/brooklyn/ProjectOne.java
+++ src/brooklyn/ProjectOne.java
@@ -3,10 +3,15 @@ package brooklyn;
 public class ProjectOne {
 	
 public static void main(String[] args) {
-	
+	int a = 7;
+	int b = 9;
 	System.out.println("Fahim please dont trick me");
 	
 	System.out.println("lakha");
+	System.out.println(addNum(a,b));
 }
 
+public static int addNum(int a, int b) {
+	return a+b;
+}
 }
